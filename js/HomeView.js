604d0f16dfc3c1197431d854ce9b65e61eeda554
CD <carsten.duetschke@gmail.com>
2015-02-04 21:28:13 +0100

    freakshow implementiert ...

--- js/HomeView.js
+++ js/HomeView.js
@@ -10,7 +10,9 @@ var HomeView = function(store) {
         
     this.findByName = function() {
         store.findByName($('.search-key').val(), function(employees) {
+            $('.freak-list').html(HomeView.li2Template(employees));
             $('.employee-list').html(HomeView.liTemplate(employees));
+            $('.freak-list').html(HomeView.li2Template(employees));
         });
     };
     
@@ -36,3 +38,4 @@ var HomeView = function(store) {
  
 HomeView.template = Handlebars.compile($("#home-tpl").html());
 HomeView.liTemplate = Handlebars.compile($("#employee-li-tpl").html());
+HomeView.li2Template = Handlebars.compile($("#freaks-li-tpl").html());
--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
 var app = {
 
-
+    registerEvents: function() {
+        var self = this;
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
+        $(window).on('hashchange', $.proxy(this.route, this));
+    },
+    
+    
+    
+    
+    
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -9,12 +35,37 @@ var app = {
         }
     },
     
-    initialize: function() {
+     initialize: function() {
         var self = this;
+        this.detailsURL = /^#employees\/(\d{1,})/;
+        this.detailsURL2 = /^#freaks\/(\d{1,})/;
+        this.registerEvents();
         this.store = new MemoryStore(function() {
-            $('body').html(new HomeView(self.store).render().el);
+            self.route();
         });
     },
+    
+    
+    route: function() {
+        var hash = window.location.hash;
+        if (!hash) {
+            $('body').html(new HomeView(this.store).render().el);
+            return;
+        }
+        var match = hash.match(app.detailsURL);
+        if (match) {
+            this.store.findById(Number(match[1]), function(employee) {
+                $('body').html(new EmployeeView(employee).render().el);
+            });
+        }
+        
+        var match = hash.match(app.detailsURL2);
+        if (match) {
+            this.store.findById(Number(match[1]), function(employee) {
+                $('body').html(new FreakView(employee).render().el);
+            });
+        }
+    }
 
     
     
